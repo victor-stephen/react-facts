907dd2e871bbc5f5bc9ba9f44e5b4b74af944714
Victor Chigozie Stephen <83133960+victor-stephen@users.noreply.github.com>
2025-01-31 07:42:15 +0100

    Initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+export default function Main() {
+  return (
+    <main className="container">
+      <h1>Fun Facts about React</h1>
+      <ul className="facts-list">
+        <li className="item">Was first released in 2013</li>
+        <li className="item">Was originally created by Jordan Walke</li>
+        <li className="item">Has well over 100K stars on Github</li>
+        <li className="item">Is maintained by Meta</li>
+        <li className="item">
+          Powers thousands of enterprise apps, inluding mobile apps
+        </li>
+      </ul>
+    </main>
+  );
+}
